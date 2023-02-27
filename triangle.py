af07bd0605c2cf42ddaea5f881daf2cae2627e80
Vedmix <u.vadim11@gmail.com>
2023-02-27 23:25:24 +0300

    Added a check for the existence of a triangle

--- triangle.py
+++ triangle.py
@@ -15,12 +15,13 @@ elif choice==1:
      BC=math.sqrt(((x2-x3)**2)+((y2-y3)**2))
      AC=math.sqrt(((x1-x3)**2)+((y1-y3)**2))
      print('Информация о треугольнике:')
-if AB == BC == AC:
-     print('Тип: "Равносторонний треугольник"')
-     print('Площадь треугольника: ', ((AB**2)*math.sqrt(3))/4)
-     print('Периметр треугольника: ', AB+BC+AC)
-elif (AB!=BC==AC) or (AB==BC!=AC) or (AB!=AC==BC):
-     print('Тип: "Равнобедренный треугольник"')
+if (AB+BC>AC) and (BC+AC>AB) and (AB+AC>BC):
+     if AB == BC == AC:
+          print('Тип: "Равносторонний треугольник"')
+          print('Площадь треугольника: ', ((AB**2)*math.sqrt(3))/4)
+          print('Периметр треугольника: ', AB+BC+AC)
+     elif (AB!=BC==AC) or (AB==BC!=AC) or (AB!=AC==BC):
+          print('Тип: "Равнобедренный треугольник"')
      if AB!=BC==AC:
           print('Площадь треугольника: ', (math.sqrt(4*(BC**2)-(AB**2)))/2)
           print('Периметр треугольника: ', AB + BC + AC)
@@ -30,22 +31,24 @@ elif (AB!=BC==AC) or (AB==BC!=AC) or (AB!=AC==BC):
      elif AB!=AC==BC:
           print('Площадь треугольника: ', (math.sqrt(4*(BC**2)-(AB**2)))/2)
           print('Периметр треугольника: ', AB + BC + AC)
-elif AB!=BC!=AC:
-     list1=[AB, BC, AC]
-     min_side=min(list1)
-     mid_side=sum(list1)-min(list1)-max(list1)
-     max_side=max(list1)
-     if max_side**2==mid_side**2+min_side**2:
-          print('Тип: "Прямоугольный треугольник"')
-          print('Площадь треугольника: ',(mid_side*min_side)/2)
-          print('Периметр треугольника: ', mid_side + min_side + max_side)
-     elif max_side**2<mid_side**2+min_side**2:
-          print('Тип: "Остроугольный треугольник"')
-          p = (mid_side + min_side + max_side) / 2
-          print('Площадь треугольника: ', math.sqrt(p * (p - min_side) * (p - mid_side) * (p - max_side)))
-          print('Периметр треугольника: ', mid_side + min_side + max_side)
-     elif max_side**2>mid_side**2+min_side**2:
-          print('Тип: "Тупоугольный треугольник"')
-          p=(mid_side+min_side+max_side)/2
-          print('Площадь треугольника: ',math.sqrt(p*(p-min_side)*(p-mid_side)*(p-max_side)))
-          print('Периметр треугольника: ',mid_side+min_side+max_side)
+     elif AB!=BC!=AC:
+          list1=[AB, BC, AC]
+          min_side=min(list1)
+          mid_side=sum(list1)-min(list1)-max(list1)
+          max_side=max(list1)
+          if max_side**2==mid_side**2+min_side**2:
+               print('Тип: "Прямоугольный треугольник"')
+               print('Площадь треугольника: ',(mid_side*min_side)/2)
+               print('Периметр треугольника: ', mid_side + min_side + max_side)
+          elif max_side**2<mid_side**2+min_side**2:
+               print('Тип: "Остроугольный треугольник"')
+               p = (mid_side + min_side + max_side) / 2
+               print('Площадь треугольника: ', math.sqrt(p * (p - min_side) * (p - mid_side) * (p - max_side)))
+               print('Периметр треугольника: ', mid_side + min_side + max_side)
+          elif max_side**2>mid_side**2+min_side**2:
+               print('Тип: "Тупоугольный треугольник"')
+               p=(mid_side+min_side+max_side)/2
+               print('Площадь треугольника: ',math.sqrt(p*(p-min_side)*(p-mid_side)*(p-max_side)))
+               print('Периметр треугольника: ',mid_side+min_side+max_side)
+else:
+     print('Такого треугольника не существует!')
