24e3e1d3817dff55345ebd7a04fa87dca5351f9a
Vedmix <u.vadim11@gmail.com>
2023-03-10 19:57:25 +0300

    update 0.01

--- triangle.py
+++ triangle.py
@@ -5,10 +5,10 @@ from colorama import init
 from colorama import Fore, Back, Style
 
 def perimeter(AB, BC, AC):
-     return float('%.2f'%(AB+BC+AC))
+     return float((AB+BC+AC))
 def square(AB, BC, AC):
      p=perimeter(AB, BC, AC)/2
-     return '%.2f'%(math.sqrt(p*(p-AB)*(p-BC)*(p-AC)))
+     return (math.sqrt(p*(p-AB)*(p-BC)*(p-AC)))
 
 init()
 print(Fore.CYAN)
@@ -24,47 +24,27 @@ y1,y2,y3=float(y1), float(y2), float(y3)
 if (x1==x2==x3) or (y1==y2==y3):
      print('Такого треугольника не существует!')
 
+fig, ax = plt.subplots()
+
+ax.text(x1-0.05,y1-0.05, 'A',style ='italic',fontsize = 15, color="purple")
+ax.text(x2-0.05,y2+0.05, 'B',style ='italic',fontsize = 15, color="purple")
+ax.text(x3+0.05,y3-0.05, 'C',style ='italic',fontsize = 15, color="purple")
+
 AB=math.sqrt(((x1-x2)**2)+((y1-y2)**2))
 BC=math.sqrt(((x2-x3)**2)+((y2-y3)**2))
 AC=math.sqrt(((x1-x3)**2)+((y1-y3)**2))
 
-list1 = [AB, BC, AC]
-min_side = min(list1)
-mid_side = sum(list1) - min(list1) - max(list1)
-max_side = max(list1)
+AF=math.sqrt(2*AC**2+2*AB**2-BC**2)/2
+BD=math.sqrt(2*AC**2+2*BC**2-AB**2)/2
+CE=math.sqrt(2*AB**2+2*BC**2-AC**2)/2
 
-print(Fore.GREEN)
+ax.text(((x2 + x3)/2)+0.05,((y2 + y3)/2), 'F',style ='italic',fontsize = 15, color="purple")
+ax.text(((x1 + x3)/2),((y1 + y3)/2)-0.05, 'D',style ='italic',fontsize = 15, color="purple")
+ax.text(((x1 + x2)/2)-0.05,((y1 + y2)/2), 'E',style ='italic',fontsize = 15, color="purple")
 
-print('Информация о треугольнике:')
-if (AB+BC>AC) and (BC+AC>AB) and (AB+AC>BC):
-     if AB == BC == AC:
-          print('Тип >> "Равносторонний треугольник"')
-     if (max_side**2==mid_side**2+min_side**2):
-          print('Тип >> "Прямоугольный треугольник"')
-     elif (AB!=BC==AC) or (AB==BC!=AC) or (AB==AC!=BC):
-          print('Тип >> Равнобедренный треугольник')
-     print('Площадь треугольника >> ', square(AB, BC, AC))
-     print('Периметр треугольника >>', perimeter(AB, BC, AC))
-
-else:
-     print('Такого треугольника не существует!')
-
-print(Fore.BLUE)
-
-print('Углы треугольника:')
 angle_BAC=degrees(acos((AC**2+AB**2-BC**2)/(2.0 * AB * AC)))
 angle_ABC=degrees(acos((AC**2+BC**2-AB**2)/(2.0 * AC * BC)))
 angle_BCA=degrees(acos((AB**2+BC**2-AC**2)/(2.0 * AB * BC)))
-print('∠BAC >> '+str(round(angle_BAC,1))+'°')
-print('∠ABC >> '+str(round(angle_ABC,1))+'°')
-print('∠BCA >> '+str(round(angle_BCA,1))+'°')
-
-print(Fore.MAGENTA)
-
-print('Стороны треугольника:')
-print('АВ >> '+str(round(AB,1))+'см')
-print('ВC >> '+str(round(BC,1))+'см')
-print('АC >> '+str(round(AC,1))+'см')
 
 #Медиана угла А
 mid_BC_x,mid_BC_y=((x2 + x3)/2),((y2 + y3)/2)
@@ -79,21 +59,61 @@ median_C_x,median_C_y=[x3,mid_AB_x],[y3,mid_AB_y]
 x,y=[x1,x2,x3],[y1,y2,y3]
 x1,y1=[x1,x3],[y1,y3]
 
-fig, ax = plt.subplots()
+print(Fore.GREEN)
+
+print('Информация о треугольнике:')
+if (AB+BC>AC) and (BC+AC>AB) and (AB+AC>BC):
+     if AB == BC == AC:
+          print('Тип >> "Равносторонний треугольник"')
+     if (angle_BAC==90) or (angle_ABC==90) or (angle_BCA==90):
+          print('Тип >> "Прямоугольный треугольник"')
+     elif (angle_BAC<90) and (angle_ABC<90) and (angle_BCA<90):
+          print('Тип >> "Остроугольный треугольник"')
+     elif (angle_BAC > 90) or (angle_ABC > 90) or (angle_BCA > 90):
+          print('Тип >> "Тупоугольный треугольник"')
+     elif (AB!=BC==AC) or (AB==BC!=AC) or (AB==AC!=BC):
+          print('Тип >> Равнобедренный треугольник')
+     print('Площадь треугольника >> '+str('%.2f' %square(AB, BC, AC))+'см')
+     print('Периметр треугольника >> '+str('%.2f' %perimeter(AB, BC, AC))+'см')
+
+else:
+     print('Такого треугольника не существует!')
+
+print(Fore.BLUE)
+
+print('Углы треугольника:')
+print('∠BAC >> '+str('%.2f' %angle_BAC)+'°')
+print('∠ABC >> '+str('%.2f' %angle_ABC)+'°')
+print('∠BCA >> '+str('%.2f' %angle_BCA)+'°')
+
+print(Fore.MAGENTA)
+
+print('Стороны треугольника:')
+print('АВ >> '+str('%.2f' %AB)+'см')
+print('ВC >> '+str('%.2f' %BC)+'см')
+print('АC >> '+str('%.2f' %AC)+'см')
+
+print(Fore.MAGENTA)
+
+print('Длины медиан:')
+print('AF >> '+str('%.2f' %AF)+'см')
+print('BD >> '+str('%.2f' %BD)+'см')
+print('CE >> '+str('%.2f' %CE)+'см')
+
 plt.plot(x,y, color='black')
 plt.plot(x1,y1, color='black')
 plt.plot(x,y,'o')
 plt.plot(x1,y1,'o')
 
-plt.plot(median_A_x,median_A_y, color='black')
+plt.plot(median_A_x,median_A_y, color='blue')
 plt.plot(median_A_x,median_A_y,'o')
 
-plt.plot(median_B_x,median_B_y, color='black')
+plt.plot(median_B_x,median_B_y, color='blue')
 plt.plot(median_B_x,median_B_y,'o')
 
-plt.plot(median_C_x,median_C_y, color='black')
+plt.plot(median_C_x,median_C_y, color='blue')
 plt.plot(median_C_x,median_C_y,'o')
 
 ax.grid()
-plt.title('Ваш треугольник:')
+plt.title( 'Ваш треугольник:')
 plt.show()
