14d7fec8a8aaa1da3c0cd0f4120bede2ac4a3223
Vedmix <u.vadim11@gmail.com>
2023-04-06 01:23:23 +0300

    Update 0.06 (added finding 4 angles)

--- triangle.py
+++ triangle.py
@@ -7,23 +7,48 @@ from colorama import Fore, Back, Style
 
 def perimeter(AB, BC, AC):
      return float((AB+BC+AC))
+
 def square(AB, BC, AC):
      p=perimeter(AB, BC, AC)/2
      return (math.sqrt(p*(p-AB)*(p-BC)*(p-AC)))
+
 def side(point_x_1,point_y_1,point_x_2,point_y_2):
     side=math.sqrt(((point_x_1 - point_x_2) ** 2) + ((point_y_1 - point_y_2) ** 2))
     return side
+
 def lenght_median(side1,side2,side3): #side3 вычитаем
     lenght_median=math.sqrt(2*side1**2+2*side2**2-side3**2)/2
     return lenght_median
+
 def angle(side1,side2,side3): #side3 вычитаем
     angle = degrees(acos((side1 ** 2 + side2 ** 2 - side3 ** 2) / (2.0 * side1 * side2)))
     return angle
+
 def equation_side(point_x_1,point_y_1,point_x_2,point_y_2):
     A =float(point_y_2 - point_y_1)
     B =float(point_x_1 - point_x_2)
     C =float(point_x_2 * point_y_1 - point_x_1 * point_y_2)
     return '('+str(A)+')'+'x + '+'('+str(B)+')'+'y + '+'('+str(C)+')'+' = 0'
+
+def line_coefficients(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+def intersection_x(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    if D != 0:
+        x = Dx / D
+        return x
+
+def intersection_y(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        y = Dy / D
+        return y
 
 while True:
 
@@ -160,16 +185,56 @@ while True:
 
         x1, x2, x3, x4 = float(x1), float(x2), float(x3), float(x4)
         y1, y2, y3, y4 = float(y1), float(y2), float(y3), float(y4)
-        AB=side(point_x_1=x1,point_y_1=y1,point_x_2=x2,point_y_2=x3)
-        BC=side(point_x_1=x2,point_y_1=y2,point_x_2=x3,point_y_2=y3)
-        CD=side(point_x_1=x3,point_y_1=y3,point_x_2=x4,point_y_2=y4)
-        AD=side(point_x_1=x1,point_y_1=y1,point_x_2=x4,point_y_2=y4)
 
-        AC=side(point_x_1=x2,point_y_1=y2,point_x_2=x4,point_y_2=x4)
-        BD=side(point_x_1=x1,point_y_1=y1,point_x_2=x3,point_y_2=x3)
+        AB = side(point_x_1=x1, point_y_1=y1, point_x_2=x2, point_y_2=y2)
+        BC = side(point_x_1=x2, point_y_1=y2, point_x_2=x3, point_y_2=y3)
+        CD = side(point_x_1=x3, point_y_1=y3, point_x_2=x4, point_y_2=y4)
+        AD = side(point_x_1=x1, point_y_1=y1, point_x_2=x4, point_y_2=y4)
 
-        #Точка пересечения диагоналей
-        mid_diag_x, mid_diag_y = ((x2+x4)/2),((y2+x4)/2)
+        AC = side(point_x_1=x2, point_y_1=y2, point_x_2=x4, point_y_2=x4)
+        BD = side(point_x_1=x1, point_y_1=y1, point_x_2=x3, point_y_2=x3)
 
+        AC1 = line_coefficients([x1, y1], [x3, y3])
+        BD1 = line_coefficients([x2, y2], [x4, y4])
+
+        O_X = intersection_x(AC1, BD1)
+        O_Y = intersection_y(AC1, BD1)
+
+        AO = side(point_x_1=x1, point_y_1=y1, point_x_2=O_X, point_y_2=O_Y)
+        BO = side(point_x_1=x2, point_y_1=y2, point_x_2=O_X, point_y_2=O_Y)
+        CO = side(point_x_1=x3, point_y_1=y3, point_x_2=O_X, point_y_2=O_Y)
+        DO = side(point_x_1=x3, point_y_1=y3, point_x_2=O_X, point_y_2=O_Y)
+
+        angle_BAO = angle(side1=AB, side2=AO, side3=BO)
+        angle_OAD = angle(side1=AO, side2=AD, side3=DO)
+        angle_BAD = angle_BAO + angle_OAD
+
+        angle_ABO = angle(side1=AB, side2=BO, side3=AO)
+        angle_CBO = angle(side1=BC, side2=BO, side3=CO)
+        angle_ABC = angle_ABO + angle_CBO
+
+        angle_BCO = angle(side1=BC, side2=CO, side3=BO)
+        angle_OCD = angle(side1=CO, side2=CD, side3=DO)
+        angle_BCD = angle_OCD + angle_BCO
+
+        angle_CDO = angle(side1=CD, side2=DO, side3=CO)
+        angle_ODA = angle(side1=DO, side2=AD, side3=AO)  # О даа...
+        angle_ADC = angle_CDO + angle_ODA
+
+        print(AB)
+        print(BC)
+        print(CD)
+        print(AD)
+
+        print(AO)
+        print(BO)
+        print(CO)
+        print(DO)
+
+        print('%.2f' % angle_BAD)
+        print('%.2f' % angle_ABC)
+        print('%.2f' % angle_BCD)
+        print('%.2f' % angle_ADC)
     elif count_points==5:
+        print()
     break
