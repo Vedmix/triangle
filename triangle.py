3eb5f2ff5895ca09a4a61497e10f9396a151b1f5
VEDMIX <vedmix.work@gmail.com>
2023-06-04 03:56:43 +0300

    Update 0.29 (minor updates)

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from math import pi
-from colorama import Fore, Back, Style, init
+from colorama import Fore, init
 from functions_triangle import*
 
 while True:
@@ -196,7 +196,6 @@ while True:
     print(f'BC >> {equationYKB(x2, y2, x3, y3)}')
     print(f'AC >> {equationYKB(x1, y1, x3, y3)}')
 
-    # print(''+str(r))
     x, y = [x1, x2, x3], [y1, y2, y3]
     x1, y1 = [x1, x3], [y1, y3]
 
